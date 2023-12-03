a51ba475ff0ab76306cd0b1e9ed535192c6efca3
Biang LI <bianglibeyond@gmail.com>
2023-12-03 19:34:34 +0800

    Added unit test code for calc

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    # Add x and y, return sum
+    return x+y
+
+
+def subtract(x, y):
+    """Subtract y from x, return the left"""
+    return x - y
